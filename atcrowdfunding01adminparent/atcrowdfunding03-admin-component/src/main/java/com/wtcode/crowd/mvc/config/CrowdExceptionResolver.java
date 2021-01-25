2318426293f6ef800654d83b8be2b2f02f6e4e10
wtcode_glo <wtcode_glo@xiaomi.com>
2021-01-25 20:09:50 +0800

    Ajax和异常映射

---
+++ atcrowdfunding01adminparent/atcrowdfunding03-admin-component/src/main/java/com/wtcode/crowd/mvc/config/CrowdExceptionResolver.java
@@ -0,0 +1,74 @@
+package com.wtcode.crowd.mvc.config;
+
+import com.google.gson.Gson;
+import com.wtcode.crowd.constant.CrowdConstant;
+import com.wtcode.crowd.util.CrowdUtil;
+import com.wtcode.crowd.util.ResultEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author wtcode
+ * @date 2021/1/25 - 15:41
+ */
+
+//@ControllerAdvice表示当前类是一个基于注解异常处理器类
+@ControllerAdvice
+public class CrowdExceptionResolver {
+
+    @ExceptionHandler(value = ArithmeticException.class)
+    public ModelAndView resolveMathException(ArithmeticException exception,HttpServletRequest request,HttpServletResponse response) throws IOException {
+        String viewName = "system-error";
+        return commonResolve(viewName,exception,request,response);
+    }
+
+
+    //@ExceptionHandler将一个具体的异常类型和一个方法关联起来
+    @ExceptionHandler(value = NullPointerException.class)
+    public ModelAndView resolveNullPointerException(
+            //实际捕获到的异常类型
+            NullPointerException exception,
+            //当前请求对象
+            HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String viewName = "system-error";
+        return commonResolve(viewName,exception,request,response);
+
+    }
+
+    private ModelAndView commonResolve(String viewName,Exception exception,HttpServletRequest request,HttpServletResponse response) throws IOException {
+        //1.判断当前请求的类型
+        boolean judgeResult = CrowdUtil.judgeRequestType(request);
+        //2.如果是Ajax请求
+        if(judgeResult){
+            //3.创建ResultEntity对象
+            ResultEntity<Object> resultEntity = ResultEntity.failed(exception.getMessage());
+            //4.创建Gson对象
+            Gson gson = new Gson();
+            //5.将ResultEntity对象转换为Json字符串
+            String json = gson.toJson(resultEntity);
+            //6.将Json字符串作为响应体返回给浏览器
+            response.getWriter().write(json);
+            //7.由于上面已经通过原生的response对象返回了响应，所以不提供ModelAndView对象
+            return null;
+        }
+
+        //8.如果不是Ajax请求，则创建ModelAndView对象
+        ModelAndView modelAndView = new ModelAndView();
+
+        //9.将Exception对象存入模型
+        modelAndView.addObject(CrowdConstant.ATTR_NAME_EXCEPTION,exception);
+
+        //10.设置对应的视图名称
+        modelAndView.setViewName(viewName);
+
+        //11.返回ModelAndView对象
+        return modelAndView;
+
+    }
+
+}
--- atcrowdfunding01adminparent/atcrowdfunding03-admin-component/src/main/java/com/wtcode/crowd/mvc/handler/TestHandler.java
+++ atcrowdfunding01adminparent/atcrowdfunding03-admin-component/src/main/java/com/wtcode/crowd/mvc/handler/TestHandler.java
@@ -1,14 +1,22 @@
 package com.wtcode.crowd.mvc.handler;
 
 import com.wtcode.crowd.entity.Admin;
+import com.wtcode.crowd.entity.ParamData;
+import com.wtcode.crowd.entity.Student;
 import com.wtcode.crowd.service.AdminService;
+import com.wtcode.crowd.util.CrowdUtil;
+import com.wtcode.crowd.util.ResultEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
@@ -21,8 +29,49 @@ public class TestHandler {
     @Autowired
     private AdminService adminService;
 
+    private Logger logger = LoggerFactory.getLogger(TestHandler.class);
+
     @ResponseBody
-    @RequestMapping("/send/array.html")
+    @RequestMapping("/send/compose/object.json")
+    public ResultEntity<Student> testReceiveComposeObject(@RequestBody Student student,HttpServletRequest request){
+
+        boolean judgeResult = CrowdUtil.judgeRequestType(request);
+
+        logger.info("judgeResult=" + judgeResult);
+        logger.info(student.toString());
+
+        //将查询到的Student对象封装到ResultEntity中返回
+        ResultEntity<Student> resultEntity = ResultEntity.successWithData(student);
+
+        return resultEntity;
+    }
+
+
+    @ResponseBody
+    @RequestMapping("/send/array/three.html")
+    public String testReceiveArrayThree(@RequestBody List<Integer> array){
+
+
+        for (Integer number : array) {
+            logger.info("number=" + number);
+        }
+        return "success";
+    }
+
+
+    @ResponseBody
+    @RequestMapping("/send/array/two.html")
+    public String testReceiveArrayTwo(ParamData paramData){
+        List<Integer> array = paramData.getArray();
+        for (Integer number : array) {
+            System.out.println("number=" + number);
+        }
+        return "success";
+    }
+
+
+    @ResponseBody
+    @RequestMapping("/send/array/one.html")
     public String testReceiveArrayOne(@RequestParam("array[]") List<Integer> array){
         for (Integer number : array) {
             System.out.println("number=" + number);
@@ -31,9 +80,20 @@ public class TestHandler {
     }
 
     @RequestMapping("/test/ssm.html")
-    public String testSsm(ModelMap modelMap){
+    public String testSsm(ModelMap modelMap, HttpServletRequest request){
+
+        boolean judgeResult = CrowdUtil.judgeRequestType(request);
+
+        logger.info("judgeResult=" + judgeResult);
+
         List<Admin> adminList = adminService.getAll();
+
         modelMap.addAttribute("adminList",adminList);
+
+        //String a = null;
+        //System.out.println(a.length());
+        System.out.println(10/0);
+
         return "target";
     }
 
